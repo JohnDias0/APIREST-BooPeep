660c0fc4bdd7bfad98ac73afc0a92f5c7db5afa8
JohnDias0 <dias44520@gmail.com>
2024-03-22 17:56:58 -0300

    feat: UpdatePost

--- src/Service/Repositories/PostRepository.ts
+++ src/Service/Repositories/PostRepository.ts
@@ -6,7 +6,7 @@ import { Post } from "../Model/Post";
 export class PostRepository {
     private db: Firestore
     private collectionPath: string
-    constructor(){
+    constructor() {
         conn()
         this.db = getFirestore()
         this.collectionPath = 'posts'
@@ -19,7 +19,7 @@ export class PostRepository {
             const collectionRef = this.db.collection('users');
             const query = collectionRef.where(field, "==", value);
             const querySnapshot = await query.get();
-            
+
             if (querySnapshot.empty) {
                 console.log("No documents found");
                 return null;
@@ -38,7 +38,7 @@ export class PostRepository {
             return null;
         }
     }
-    async findByID(id: string): Promise<Post |null>{
+    async findByID(id: string): Promise<Post | null> {
         const field = 'id';
         const value = id;
 
@@ -46,7 +46,7 @@ export class PostRepository {
             const collectionRef = this.db.collection(this.collectionPath);
             const query = collectionRef.where(field, "==", value);
             const querySnapshot = await query.get();
-            
+
             if (querySnapshot.empty) {
                 console.log("No documents found");
                 return null;
@@ -65,7 +65,7 @@ export class PostRepository {
             return null;
         }
     }
-    async getAllPosts(): Promise<Post[] | null>{
+    async getAllPosts(): Promise<Post[] | null> {
         try {
             const collectionRef = this.db.collection(this.collectionPath);
             const querySnapshot = await collectionRef.get();
@@ -74,7 +74,7 @@ export class PostRepository {
                 const postData = doc.data() as Post;
                 posts.push(postData);
             });
-            if(posts[1] === null){
+            if (posts[1] === null) {
                 console.log('Nenhum post encontrado')
                 return null
             }
@@ -84,8 +84,8 @@ export class PostRepository {
             return null;
         }
     }
-    
-    
+
+
     async save(post: Post): Promise<void> {
         try {
             const docRef: DocumentData = await this.db.collection(this.collectionPath).add(post);
@@ -95,4 +95,40 @@ export class PostRepository {
             console.error(`Erro ao cadastrar o usuário: ${error}`);
         }
     }
+    
+
+    async updatePostField(postId: string, fieldToUpdate: string, newValue: any): Promise<void | string > {
+        try {
+            const postRef = this.db.collection(this.collectionPath).doc(postId);
+
+            const postSnapshot = await postRef.get();
+
+            if (!postSnapshot.exists) {
+                console.error('Documento não encontrado.');
+                return 'Documento não encontrado';
+            }
+    
+            const postData = postSnapshot.data();
+            if (!postData || !postData.hasOwnProperty(fieldToUpdate)) {
+                console.error(`O campo '${fieldToUpdate}' não existe no documento.`);
+                return 'O campo requerido não existe no documento';
+            }
+    
+            const previousValue = postData[fieldToUpdate];
+            if (typeof previousValue !== typeof newValue) {
+                console.error(`O tipo do valor anterior '${previousValue}' não corresponde ao tipo do novo valor '${newValue}'.`);
+                return 'O tipo do valor anterior do campo requerido não corresponde ao tipo do novo valor ';
+            }
+    
+            await postRef.update({
+                [fieldToUpdate]: newValue
+            });
+    
+            console.log('Campo atualizado com sucesso.');
+            return 'Campo atualizado com sucesso';
+        } catch (error) {
+            console.error('Erro ao atualizar campo:', error);
+        }
+    }
+
 }
